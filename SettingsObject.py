06e6c999140b96b7c2473eb666b0f2b1558d05a5
Andrea Gennusa <andrea.gennusa@gmail.com>
2017-07-20 17:42:40 +0200

    added settings

--- SettingsObject.py
+++ SettingsObject.py
@@ -63,6 +63,10 @@ class SettingsObject(object):
     def get_verbosity(self):
         return int(self.configDict.get("verbosity")[0])
 
+    def get_pause_duration(self):
+        return int(self.configDict.get("pause_duration")[0])
+
+
     def __str__(self):
         string = "Configuration:\n"
         for (key,value) in self.configDict.iteritems() :
