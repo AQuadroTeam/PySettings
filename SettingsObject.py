be2f38dd4fa94537c0ed80a32c878cfd53c12135
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-07-28 17:27:58 +0200

    Added get_random_port, added id_counter, added configure function to store events to monitor, added event_ids in settings.txt

--- SettingsObject.py
+++ SettingsObject.py
@@ -50,7 +50,7 @@ class SettingsObject(object):
         return str(self.configDict.get("max_port")[0])
 
     def get_event_ids(self):
-        return str(self.configDict.get("event_ids"))
+        return self.configDict.get("event_ids")
 
     def get_station_name(self):
         return str(self.configDict.get("station_name")[0])
